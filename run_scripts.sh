b408b8916affecfbfbc5b4fae94571a048b8c2e8
afi3008 <annalenafischer3099@gmail.com>
2026-04-21 15:20:14 +0200

    Examples on how to run scripts

---
+++ run_scripts.sh
@@ -0,0 +1,3 @@
+python cluster_kmeansbatch.py --pdb_0 /path/to/structure_z_0.pdb --path_to_pdb /path/to/predicted_structures/ --output_path combined_all_structures.pdb --sele AtomSelection
+
+pyhon cluster_extract_starfile.py --path_starfile /path/to/starfile --mapping_path /path/to/cluster/structure/mapping --prefix_output_starfile prefix --stride 1
